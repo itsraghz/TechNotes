224e74dd46a40f6d55a9b2c8176a88c4eef6e982
Raghavan alias Saravanan Muthu <raghavan.muthu@outlook.com>
2020-12-28 13:20:03 +0530

    [RaghsLenovoLegion] | Groovy Materials - WIP - Automated Checkin

---
+++ groovyNotes/features/javabeans/BookBean.java
@@ -0,0 +1,21 @@
+public class BookBean
+{
+    private String title;
+
+    public BookBean(String title)
+    {
+	    this.title=title;
+	    System.out.println("BookBean instantiated");
+    }
+
+    public String getTitle()
+    {
+	    return this.title;
+    }
+
+    @Override
+    public String toString()
+    {
+	    return "[Book] hashcode=" + this.hashCode() + ", title="+this.title;
+    }
+}
---
+++ groovyNotes/features/javabeans/versions/BookBeanV1.java
@@ -0,0 +1,5 @@
+public class BookBean
+{
+    private String title;
+
+}
