e9c4b8d7d1ddfe92d22f4e8ad389a74cf069c30f
Raghavan alias Saravanan M <raghavan.muthu@outlook.com>
2021-06-22 02:03:57 +0530

    typescript dyamic arrays program added

---
+++ typeScriptPractices/03-loops-and-arrays/dynamic-arrays.js
@@ -0,0 +1,7 @@
+var subjects = ["C", "C++", "Java"];
+subjects.push("Python");
+subjects.push("AWS");
+for (var _i = 0, subjects_1 = subjects; _i < subjects_1.length; _i++) {
+    var subject = subjects_1[_i];
+    console.log(subject);
+}
---
+++ typeScriptPractices/03-loops-and-arrays/dynamic-arrays.ts
@@ -0,0 +1,8 @@
+let subjects : string[] = ["C", "C++", "Java"];
+
+subjects.push("Python");
+subjects.push("AWS");
+
+for(let subject of subjects) {
+    console.log(subject);
+}
